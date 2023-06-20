202d6cd06c79eb9de60cb4b9c628a6f1d82ffe3a
Leuel-Tesfaye <131950742+Leuel-Tesfaye@users.noreply.github.com>
2023-06-20 10:18:38 +0300

    exercise level solved up to question 2

--- exercise.js
+++ exercise.js
@@ -54,6 +54,24 @@ const russia = countries.findIndex((russia) => russia.includes('russia'))
 console.log(russia)
 
 // exercise level 2 
+// 1 . Find the total price of products by chaining two or more array iterators(eg. arr.map(callback).filter(callback).reduce(callback))
 
+const totalPrice = products.filter((item) => item.price !== '' && item.price !== ' ').reduce((acc,item) =>
+ acc + item.price,0)
+ console.log(totalPrice)
 
+ // 2. Find the sum of price of products using only reduce reduce(callback))
+const sum = products.reduce((acc, item) => {
+   if(item.price && typeof item.price === 'number'){
+      return acc + item.price 
+   }
+   return acc
+}, 0)
+console.log(sum)
 
+//3. Declare a function called categorizeCountries which returns an array of countries which have some common pattern(you find the countries array in this repository as countries.js(eg 'land', 'ia', 'island','stan')).
+const categorizeCountries = () =>{
+   const countriesWithLand = countries.some((countriesWithLand) => countriesWithLand.includes('land'))
+   console.log(countriesWithLand)
+}
+categorizeCountries() 
